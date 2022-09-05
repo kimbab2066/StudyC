54389b22402de873a06ec429e7a7937c319512fa
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-09-05 20:08:02 +0900

    Chap2

---
+++ 2장/2장.c
@@ -0,0 +1,17 @@
+#include<stdio.h>//standard inputoutput을 의미(표준입출력)
+
+
+int main(void) {
+	10 + 20;
+	printf("hello\t world \n");
+	printf("world\n");
+	printf("Good\bd\tchance\n");
+	printf("Cow\rW\a\n");
+
+	printf("%d\n", 10);
+	printf("%.2lf\n", 3.14);
+	//printf("변환 문자",숫자)
+
+
+	return 0;
+}
