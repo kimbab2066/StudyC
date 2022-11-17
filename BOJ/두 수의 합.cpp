7a1603610f30c8ca4eb11a2069f423fe2d6f0960
kimbab2066 <qseft523125@g.yju.ac.kr>
2022-11-17 20:27:50 +0900

    두 수의 합

--- BOJ/두 수의 합.cpp
+++ BOJ/두 수의 합.cpp
@@ -2,9 +2,23 @@
 #include <algorithm>
 #include <string>
 using namespace std;
+int arr[2000001] = {};
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
+	int n, x, val, cnt = 0;
+	cin >> n;
 	
+	for (int i = 0; i < n; i++) {
+		cin >> val;
+		arr[val]++;
+	}
+	cin >> x;
+
+	for (int i = 1; i < (x+1)/2; i++) {
+		if (arr[i] == 1 && arr[x - i] == 1)cnt++;
+	}
+	cout << cnt;
 }
