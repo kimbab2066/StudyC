5a16df5ee1daea44010d057ea01beb96a7b8225b
kimbab2066 <qseft523125@g.yju.ac.kr>
2023-03-07 19:28:48 +0900

    프로그래머스 & 삼성문제풀이

--- BOJ/13460.cpp
+++ BOJ/13460.cpp
@@ -4,37 +4,49 @@
 using namespace std;
 
 char map[11][11];
-int a[11][11], chk[11][11];
-int q[101][2], d[101];
+int a[11][11], b[11][11];
+int q[101][2];
 int n, m;
 int st, ed, res, sw;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-
 struct tmp {
 	int x, y;
-}k[2];
+}k[3];
 int inRange(int x, int y) {
 	return 0 <= x && x < n && 0 <= y && y < m;
 }
-void bfs() {
+
+void bfs(tmp k[]) {
+	//Q초기화
+	st = ed = -1;
+	q[++st][0] = k[0].x;
+	q[st][1] = k[0].y;
+
 	while (st != ed) {
+		//Q에서 값 빼기
 		int x = q[++ed][0];
 		int y = q[ed][1];
-		if (map[x][y] == 'O') { sw = 1; break; }
-		for (int d = 0; d < 4; d++) {
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			for (int i = 1; i <= 10; i++) {
-				if (inRange(nx, ny) && chk[nx][ny] == 0 && map[nx][ny] != 'B') {
-					chk[i * nx][i * ny] = 1;
-					a[i * nx][i * ny] = a[x][y] + 1;
+		//종료 조건
+		if (map[x][y] == 'O') break;
+		//Red or Blue
+		int NM = n > m ? n : m;
+		if (map[i][j] == 'R') {
+			for (int d = 0; d < 4; d++) {
+				for (int k = 1; k <= NM; k++) {
+					int nx = x + dx[d] * k;
+					int ny = y + dy[d] * k;
+					if (inRange(nx, ny) && map[nx][ny] != '#' && a[nx][ny] == 0) {
+						q[++st][0] = nx;
+						q[st][1] = ny;
+						a[nx][ny] = a[x][y] + 1;
+					}
+					else break;
 				}
-				else break;
-			}//i
-		}//d
+			}//d
+		}
 	}//w
-}
+}//bfs
 
 int main(void) {
 	//input
@@ -43,26 +55,19 @@ int main(void) {
 	for (i = 0; i < n; i++) {
 		for (j = 0; j < m; j++) {
 			cin >> map[i][j];
-			if (map[i][j] == 'R') {
-				k[0].x = i;
-				k[0].y = j;
-			}
-			else if (map[i][j] == 'B') {
-				k[1].x = i;
-				k[1].y = j;
-			}
 		}
 	}
 	//logic
-	st = ed = -1;
-	q[++st][0] = k[0].x;
-	q[st][1] = k[0].y;
-	d[st] = 0;
-	chk[k[0].x][k[0].y] = 1;
-	bfs();
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			if (map[i][j] == 'R') { k[0].x = i; k[0].y = j; }
+			else if (map[i][j] == 'B') { k[1].x = i; k[1].y = j; }
+			else if (map[i][j] == 'O') { k[2].x = i; k[2].y = j; }
+		}
+	}
+	bfs(k);
 	//output
-	if (sw == 1) cout << res;
-	else cout << -1;
+	cout << res;
 
 	return 0;
 }//main
